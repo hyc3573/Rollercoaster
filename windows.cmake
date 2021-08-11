a1a83d3226c5eae247b2a372292e684922b78c5a
FINDLASTELEMENT <fgh134134@naver.com>
2021-08-11 16:33:14 +0900

    윈도우 빌드 지원

---
+++ windows.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+set(CMAKE_INCLUDE_DIR "/usr/x86_64-w64-mingw32/include")
+include_directories(/usr/x86_64-w64-mingw32/include)
+set(CMAKE_LIBRARY_DIR /usr/x86_64-w64-mingw32/lib)
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES "-s.a")
+set(BUILD_SHARED_LIBS OFF)
+set(CMAKE_EXE_LINKER_FLAGS "-static")
+
